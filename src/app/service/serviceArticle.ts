3841b7edf3b9d1e266c070416bb909f96f2bf15e
Germainita <dioufgermainedaba@gamil.com>
2023-11-22 10:15:23 +0000

    Service article

---
+++ src/app/service/serviceArticle.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class ServiceArticle {
+    // Injection de dépendance de la classe HttpClient
+    constructor(private httpClient: HttpClient){}
+
+    // Methode pour récupérer les articles 
+    getArticle(){
+        this.httpClient.get("https://jsonplaceholder.typicode.com/posts");
+    }
+}
